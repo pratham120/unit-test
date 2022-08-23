a863eed67cc275e2feb6b90e9c029d65e6a195c2
pratham120 <88429570+pratham120@users.noreply.github.com>
2022-08-23 13:37:07 +0530

    Delete patientmanagementsystem directory

--- patientmanagementsystem/src/main/java/com/example/patientmanagementsystem/repository/PatientRecordRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.patientmanagementsystem.repository;
-
-import com.example.patientmanagementsystem.model.PatientRecord;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PatientRecordRepository extends JpaRepository<PatientRecord, Integer>{
-}
